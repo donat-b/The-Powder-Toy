bee509784de4ba941301404ef864fc35ecab26dc
jacob1 <jfu614@gmail.com>
2013-01-08 10:41:18 -0500

    tpt++: fix GOL detection with DTEC

--- src/elements/dtec.c
+++ src/elements/dtec.c
@@ -49,7 +49,7 @@ int update_DTEC(UPDATE_FUNC_ARGS) {
 					r = photons[y+ry][x+rx];
 				if (!r)
 					continue;
-				if (parts[r>>8].type == parts[i].ctype && (parts[i].ctype != PT_LIFE || parts[i].tmp == parts[r>>8].tmp))
+				if (parts[r>>8].type == parts[i].ctype && (parts[i].ctype != PT_LIFE || parts[i].tmp == parts[r>>8].ctype || !parts[i].tmp))
 					parts[i].life = 1;
 			}
 	return 0;
