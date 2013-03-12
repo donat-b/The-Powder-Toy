2be6cce74dd63d0a78d67a87d28ab636a439d951
jacob1 <jfu614@gmail.com>
2013-03-11 23:14:28 -0400

    tpt++: the latest cray changes too

--- src/elements/cray.c
+++ src/elements/cray.c
@@ -42,9 +42,8 @@ unsigned int wavelengthToDecoColour(int wavelength)
 
 int update_CRAY(UPDATE_FUNC_ARGS)
 {
-	int r, nxx, nyy, docontinue, nxi, nyi, rx, ry, nr, ry1, rx1, partsRemaining = 255;
-	if (parts[i].tmp)
-		partsRemaining = parts[i].tmp;
+	int r, nxx, nyy, docontinue, nxi, nyi, rx, ry, nr, ry1, rx1;
+	// set ctype to things that touch it if it doesn't have one already
 	if(parts[i].ctype<=0 || parts[i].ctype>=PT_NUM || !ptypes[parts[i].ctype].enabled) {
 		int r, rx, ry;
 		for (rx=-1; rx<2; rx++)
@@ -56,13 +55,13 @@ int update_CRAY(UPDATE_FUNC_ARGS)
 						r = pmap[y+ry][x+rx];
 					if (!r)
 						continue;
-					if ((r&0xFF)!=PT_CRAY && (r&0xFF)<PT_NUM)
+					if ((r&0xFF)!=PT_CRAY && (r&0xFF)!=PT_PSCN && (r&0xFF)!=PT_INST && (r&0xFF)!=PT_METL && (r&0xFF)!=PT_SPRK && (r&0xFF)<PT_NUM)
 					{
 						parts[i].ctype = r&0xFF;
+						parts[i].temp = parts[r>>8].temp;
 					}
 				}
-	} else if (parts[i].life==0) {
-		unsigned int colored = 0;
+	} else if (parts[i].life==0) { // only fire when life is 0, but nothing sets the life right now
 		for (rx=-1; rx<2; rx++)
 			for (ry=-1; ry<2; ry++)
 				if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
@@ -70,32 +69,44 @@ int update_CRAY(UPDATE_FUNC_ARGS)
 					r = pmap[y+ry][x+rx];
 					if (!r)
 						continue;
-					if ((r&0xFF)==PT_SPRK && parts[r>>8].life==3) {
-						int destroy = (parts[r>>8].ctype==PT_PSCN)?1:0;
-						int nostop = (parts[r>>8].ctype==PT_INST)?1:0;
+					if ((r&0xFF)==PT_SPRK && parts[r>>8].life==3) { //spark found, start creating
+						unsigned int colored = 0;
+						int destroy = parts[r>>8].ctype==PT_PSCN;
+						int nostop = parts[r>>8].ctype==PT_INST;
+						int createSpark = (parts[r>>8].ctype==PT_INWR);
+						int partsRemaining = 255;
+						if (parts[i].tmp) //how far it shoots
+							partsRemaining = parts[i].tmp;
 						for (docontinue = 1, nxx = 0, nyy = 0, nxi = rx*-1, nyi = ry*-1; docontinue; nyy+=nyi, nxx+=nxi) {
 							if (!(x+nxi+nxx<XRES && y+nyi+nyy<YRES && x+nxi+nxx >= 0 && y+nyi+nyy >= 0)) {
 								break;
 							}
 							r = pmap[y+nyi+nyy][x+nxi+nxx];
-							if (!r) {
-								int nr = create_part(-1, x+nxi+nxx, y+nyi+nyy, parts[i].ctype);
+							if (!IsWallBlocking(x+nxi+nxx, y+nyi+nyy, parts[i].ctype) && (!pmap[y+nyi+nyy][x+nxi+nxx] || createSpark)) { // create, also set color if it has passed through FILT
+								int nr;
+								if (parts[i].ctype == PT_LIFE)
+									nr = create_part(-1, x+nxi+nxx, y+nyi+nyy, parts[i].ctype|(parts[i].tmp2<<8));
+								else
+									nr = create_part(-1, x+nxi+nxx, y+nyi+nyy, parts[i].ctype);
 								if (nr!=-1) {
 									parts[nr].dcolour = colored;
+									parts[nr].temp = parts[i].temp;
 									if(!--partsRemaining)
 										docontinue = 0;
 								}
-							} else if ((r&0xFF)==PT_FILT) {//get color if passed through FILT
+							} else if ((r&0xFF)==PT_FILT) { // get color if passed through FILT
 								colored = wavelengthToDecoColour(parts[r>>8].ctype);
-							} else if ((r&0xFF)==PT_CRAY || nostop) {
+							} else if ((r&0xFF) == PT_CRAY || nostop) {
 								docontinue = 1;
-							} else if(destroy && (r&0xFF) != PT_DMND) {
+							} else if(destroy && ((r&0xFF) != PT_DMND)) {
 								kill_part(r>>8);
 								if(!--partsRemaining)
 									docontinue = 0;
-							} else {
-								docontinue = 0;
 							}
+							else
+								docontinue = 0;
+							if(!partsRemaining)
+								docontinue = 0;
 						}
 					}
 				}
