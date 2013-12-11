7acfb8229f1fd09330297b19b8e212821997fea7
jacob1 <jfu614@gmail.com>
2013-12-10 21:45:26 -0500

    tpt++/cracker64: Some better comments in SPRK, renamed rt and ct. 5e1c7d7932ae

--- src/simulation/elements/SPRK.cpp
+++ src/simulation/elements/SPRK.cpp
@@ -19,7 +19,7 @@ int NPTCT_update(UPDATE_FUNC_ARGS);
 
 int SPRK_update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry, rt, nearp, pavg, ct = parts[i].ctype, rd = 2;
+	int r, rx, ry, nearp, pavg, ct = parts[i].ctype, sender, receiver, rd = 2;
 	update_PYRO(UPDATE_FUNC_SUBCALL_ARGS);
 
 	if (parts[i].life<=0)
@@ -46,6 +46,8 @@ int SPRK_update(UPDATE_FUNC_ARGS)
 			parts[i].life = 14;
 		return 0;
 	}
+
+	//Some functions of SPRK based on ctype (what it is on)
 	switch (ct)
 	{
 	case PT_SPRK:
@@ -151,23 +153,27 @@ int SPRK_update(UPDATE_FUNC_ARGS)
 				r = pmap[y+ry][x+rx];
 				if (!r)
 					continue;
-				rt = r&0xFF;
 
-				// ct = spark from material, rt = spark to material
-
+				//receiver is the element SPRK is trying to conduct to
+				//sender is the element the SPRK is on
+				//pavg is the element in the middle of them both
+				receiver = r&0xFF;
+				sender = ct;
 				pavg = parts_avg(r>>8, i,PT_INSL);
-				switch (rt)
+
+				//First, some checks usually for (de)activation of elements
+				switch (receiver)
 				{
 				case PT_SWCH:
 				case PT_BUTN:
 					// make sparked SWCH and BUTN turn off correctly
 					if (!(parts[i].flags & FLAG_INSTACTV) && pavg != PT_INSL && parts[i].life < 4)
 					{
-						if (ct == PT_PSCN && parts[r>>8].life<10)
+						if (sender == PT_PSCN && parts[r>>8].life<10)
 						{
 							parts[r>>8].life = 10;
 						}
-						else if (ct == PT_NSCN)
+						else if (sender == PT_NSCN)
 						{
 							parts[r>>8].ctype = PT_NONE;
 							parts[r>>8].life = 9;
@@ -179,7 +185,7 @@ int SPRK_update(UPDATE_FUNC_ARGS)
 					{
 						if (parts[r>>8].ctype == PT_SWCH || parts[r>>8].ctype == PT_BUTN)
 						{
-							if (!(parts[i].flags & FLAG_INSTACTV) && ct == PT_NSCN)
+							if (!(parts[i].flags & FLAG_INSTACTV) && sender == PT_NSCN)
 							{
 								part_change_type(r>>8, x+rx, y+ry, parts[r>>8].ctype);
 								parts[r>>8].ctype = PT_NONE;
@@ -188,7 +194,7 @@ int SPRK_update(UPDATE_FUNC_ARGS)
 						}
 						else if (parts[r>>8].ctype==PT_NTCT || parts[r>>8].ctype==PT_PTCT)
 						{
-							if (ct == PT_METL)
+							if (sender == PT_METL)
 								parts[r>>8].temp = 473.0f;
 						}
 					}
@@ -199,35 +205,35 @@ int SPRK_update(UPDATE_FUNC_ARGS)
 				case PT_PBCN:
 					if (!(parts[i].flags & FLAG_INSTACTV) && parts[i].life < 4) // PROP_PTOGGLE, Maybe? We seem to use 2 different methods for handling actived elements, this one seems better. Yes, use this one
 					{
-						if (ct==PT_PSCN)
+						if (sender==PT_PSCN)
 							parts[r>>8].life = 10;
-						else if (ct==PT_NSCN && parts[r>>8].life>=10)
+						else if (sender==PT_NSCN && parts[r>>8].life>=10)
 							parts[r>>8].life = 9;
 					}
 					break;
 				case PT_LCRY:
 					if (abs(rx) < 2 && abs(ry) < 2 && parts[i].life < 4)
 					{
-						if (ct==PT_PSCN && parts[r>>8].tmp == 0)
+						if (sender==PT_PSCN && parts[r>>8].tmp == 0)
 							parts[r>>8].tmp = 2;
-						else if (ct==PT_NSCN && parts[r>>8].tmp == 3)
+						else if (sender==PT_NSCN && parts[r>>8].tmp == 3)
 							parts[r>>8].tmp = 1;
 					}
 					break;
 				case PT_PPIP:
 					if (parts[i].life == 3 && pavg!=PT_INSL)
 					{
-						if (ct == PT_NSCN || ct == PT_PSCN || ct == PT_INST)
-							PPIP_flood_trigger(x+rx, y+ry, ct);
+						if (sender == PT_NSCN || sender == PT_PSCN || sender == PT_INST)
+							PPIP_flood_trigger(x+rx, y+ry, sender);
 					}
 					break;
 				case PT_NTCT:
 				case PT_PTCT:
 				case PT_INWR:
-					if (ct==PT_METL && pavg!=PT_INSL && parts[i].life<4)
+					if (sender==PT_METL && pavg!=PT_INSL && parts[i].life<4)
 					{
 						parts[r>>8].temp = 473.0f;
-						if (rt==PT_NTCT || rt==PT_PTCT)
+						if (receiver==PT_NTCT || receiver==PT_PTCT)
 							continue;
 					}
 					break;
@@ -235,69 +241,72 @@ int SPRK_update(UPDATE_FUNC_ARGS)
 					break;
 				}
 
-				//the crazy conduct checks
-				if (pavg == PT_INSL)
+				if (pavg == PT_INSL) //Insulation blocks everything past here
 					continue;
-				if (!((ptypes[rt].properties&PROP_CONDUCTS) || rt==PT_INST || rt==PT_QRTZ))
+				if (!((ptypes[receiver].properties&PROP_CONDUCTS) || receiver==PT_INST || receiver==PT_QRTZ)) //Stop non-conducting recievers, allow INST and QRTZ as special cases
 					continue;
-				if (abs(rx)+abs(ry)>=4 && rt!=PT_SWCH && ct!=PT_SWCH && ct!=PT_COND)
+				if (abs(rx)+abs(ry)>=4 && receiver!=PT_SWCH && sender!=PT_SWCH && sender!=PT_COND) //Only SWCH and COND conduct really far
 					continue;
-				if (rt == ct && rt != PT_INST && rt != PT_COND)
+				if (receiver == sender && receiver != PT_INST && receiver != PT_COND) //Everything conducts to itself, except INST and COND
 					goto conduct;
 
-				switch (ct)
+				//Sender cases, where elements can have specific outputs
+				switch (sender)
 				{
 				case PT_INST:
-					if (rt == PT_NSCN)
+					if (receiver == PT_NSCN)
 						goto conduct;
 					continue;
 				case PT_SWCH:
 				case PT_BUTN:
-					if (rt==PT_PSCN||rt==PT_NSCN||rt==PT_WATR||rt==PT_SLTW||rt==PT_NTCT||rt==PT_PTCT||rt==PT_INWR)
+					if (receiver==PT_PSCN || receiver==PT_NSCN || receiver==PT_WATR || receiver==PT_SLTW || receiver==PT_NTCT || receiver==PT_PTCT || receiver==PT_INWR)
 						continue;
 					goto conduct;
 				case PT_ETRD:
-					if (rt==PT_METL||rt==PT_BMTL||rt==PT_BRMT||rt==PT_LRBD||rt==PT_RBDM||rt==PT_PSCN||rt==PT_NSCN)
+					if (receiver==PT_METL || receiver==PT_BMTL || receiver==PT_BRMT || receiver==PT_LRBD || receiver==PT_RBDM || receiver==PT_PSCN || receiver==PT_NSCN)
 						goto conduct;
 					continue;
 				case PT_NTCT:
-					if (rt==PT_PSCN || (rt==PT_NSCN&&parts[i].temp>373.0f))
+					if (receiver==PT_PSCN || (receiver==PT_NSCN && parts[i].temp>373.0f))
 						goto conduct;
 					continue;
 				case PT_PTCT:
-					if (rt==PT_PSCN || (rt==PT_NSCN&&parts[i].temp<373.0f))
+					if (receiver==PT_PSCN || (receiver==PT_NSCN && parts[i].temp<373.0f))
 						goto conduct;
 					continue;
 				case PT_INWR:
-					if (rt==PT_NSCN || rt==PT_PSCN)
+					if (receiver==PT_NSCN || receiver==PT_PSCN)
 						goto conduct;
 					continue;
 				case PT_COND:
-					if (rt == PT_COND && parts[i].tmp != parts[r>>8].tmp)
+					if (receiver == PT_COND && parts[i].tmp != parts[r>>8].tmp)
 						continue;
 					goto conduct;
+				default:
+					break;
 				}
 
-				switch (rt)
+				//Receiving cases, where elements can have specific inputs
+				switch (receiver)
 				{
 				case PT_QRTZ:
-					if ((ct==PT_NSCN||ct==PT_METL||ct==PT_PSCN) && (parts[r>>8].temp<173.15||pv[(y+ry)/CELL][(x+rx)/CELL]>8))
+					if ((sender==PT_NSCN||sender==PT_METL||sender==PT_PSCN) && (parts[r>>8].temp<173.15||pv[(y+ry)/CELL][(x+rx)/CELL]>8))
 						goto conduct;
 					continue;
 				case PT_NTCT:
-					if (ct==PT_NSCN || (ct==PT_PSCN&&parts[r>>8].temp>373.0f))
+					if (sender==PT_NSCN || (sender==PT_PSCN && parts[r>>8].temp>373.0f))
 						goto conduct;
 					continue;
 				case PT_PTCT:
-					if (ct==PT_NSCN || (ct==PT_PSCN&&parts[r>>8].temp<373.0f))
+					if (sender==PT_NSCN || (sender==PT_PSCN && parts[r>>8].temp<373.0f))
 						goto conduct;
 					continue;
 				case PT_INWR:
-					if (ct==PT_NSCN || ct==PT_PSCN)
+					if (sender==PT_NSCN || sender==PT_PSCN)
 						goto conduct;
 					continue;
 				case PT_INST:
-					if (ct == PT_PSCN)
+					if (sender == PT_PSCN)
 						goto conduct;
 					continue;
 				case PT_NBLE:
@@ -305,23 +314,28 @@ int SPRK_update(UPDATE_FUNC_ARGS)
 						goto conduct;
 					continue;
 				case PT_PSCN:
-					if (ct != PT_NSCN)
+					if (sender != PT_NSCN)
 						goto conduct;
 					continue;
+				default:
+					break;
 				}
 
 conduct:
-				if (rt==PT_WATR||rt==PT_SLTW)
+				//Passed normal conduction rules, check a few last things and change receiver to spark
+				if (receiver==PT_WATR || receiver==PT_SLTW)
 				{
 					if (parts[r>>8].life==0 && parts[i].life<3)
 					{
 						part_change_type(r>>8,x+rx,y+ry,PT_SPRK);
-						if (rt==PT_WATR) parts[r>>8].life = 6;
-						else parts[r>>8].life = 5;
-						parts[r>>8].ctype = rt;
+						if (receiver == PT_WATR)
+							parts[r>>8].life = 6;
+						else
+							parts[r>>8].life = 5;
+						parts[r>>8].ctype = receiver;
 					}
 				}
-				else if (rt==PT_INST)
+				else if (receiver == PT_INST)
 				{
 					if (parts[r>>8].life==0 && parts[i].life<4)
 					{
@@ -331,18 +345,18 @@ conduct:
 				else if (parts[r>>8].life==0 && parts[i].life<4)
 				{
 					parts[r>>8].life = 4;
-					parts[r>>8].ctype = rt;
+					parts[r>>8].ctype = receiver;
 					part_change_type(r>>8,x+rx,y+ry,PT_SPRK);
-					if (parts[r>>8].temp+10.0f<673.0f&&!legacy_enable&&(rt==PT_METL||rt==PT_BMTL||rt==PT_BRMT||rt==PT_PSCN||rt==PT_NSCN||rt==PT_ETRD||rt==PT_NBLE||rt==PT_IRON))
+					if (parts[r>>8].temp+10.0f<673.0f && !legacy_enable && (receiver==PT_METL||receiver==PT_BMTL||receiver==PT_BRMT||receiver==PT_PSCN||receiver==PT_NSCN||receiver==PT_ETRD||receiver==PT_NBLE||receiver==PT_IRON))
 						parts[r>>8].temp = parts[r>>8].temp+10.0f;
 				}
-				else if (ct==PT_ETRD && parts[i].life==5)
+				else if (sender==PT_ETRD && parts[i].life==5) //ETRD is odd and conducts to others only at life 5, this could probably be somewhere else
 				{
-					part_change_type(i,x,y,ct);
+					part_change_type(i,x,y,sender);
 					parts[i].ctype = PT_NONE;
 					parts[i].life = 20;
 					parts[r>>8].life = 4;
-					parts[r>>8].ctype = rt;
+					parts[r>>8].ctype = receiver;
 					part_change_type(r>>8,x+rx,y+ry,PT_SPRK);
 				}
 			}
