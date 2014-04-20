478ea1208aab2a70af2634c64f91b4d6a25d4e93
jacob1 <jfu614@gmail.com>
2014-04-20 19:50:05 -0400

    clean up update_legacy_all (make it look modern)

--- src/simulation/ElementFunctions.cpp
+++ src/simulation/ElementFunctions.cpp
@@ -4,83 +4,62 @@
 int update_legacy_all(UPDATE_FUNC_ARGS)
 {
 	int r, rx, ry;
-	int t = parts[i].type;
-	if (!legacy_enable)
-		return 0;
-	if (t==PT_WTRV)
+	switch (parts[i].type)
 	{
+	case PT_WTRV:
 		for (rx=-2; rx<3; rx++)
 			for (ry=-2; ry<3; ry++)
-				if (x+rx>=0 && y+ry>0 &&
-						x+rx<XRES && y+ry<YRES && (rx || ry))
+				if (BOUNDS_CHECK && (rx || ry))
 				{
 					r = pmap[y+ry][x+rx];
 					if (!r)
 						continue;
-					if (((r&0xFF)==PT_WATR||(r&0xFF)==PT_DSTW||(r&0xFF)==PT_SLTW) && 1>(rand()%1000))
+					if (((r&0xFF)==PT_WATR||(r&0xFF)==PT_DSTW||(r&0xFF)==PT_SLTW) && !(rand()%1000))
 					{
 						part_change_type(i,x,y,PT_WATR);
 						part_change_type(r>>8,x+rx,y+ry,PT_WATR);
 					}
-					if (((r&0xFF)==PT_ICEI || (r&0xFF)==PT_SNOW) && 1>(rand()%1000))
+					if (((r&0xFF)==PT_ICEI || (r&0xFF)==PT_SNOW) && !(rand()%1000))
 					{
 						part_change_type(i,x,y,PT_WATR);
-						if (1>(rand()%1000))
+						if (!(rand()%1000))
 							part_change_type(r>>8,x+rx,y+ry,PT_WATR);
 					}
 				}
-	}
-	else if (t==PT_WATR)
-	{
+		if (pv[y/CELL][x/CELL] > 4.0f)
+			part_change_type(i,x,y,PT_DSTW);
+		break;
+	case PT_WATR:
+	case PT_DSTW:
 		for (rx=-2; rx<3; rx++)
 			for (ry=-2; ry<3; ry++)
-				if (x+rx>=0 && y+ry>0 &&
-						x+rx<XRES && y+ry<YRES && (rx || ry))
+				if (BOUNDS_CHECK && (rx || ry))
 				{
 					r = pmap[y+ry][x+rx];
 					if (!r)
 						continue;
-					if (((r&0xFF)==PT_FIRE || (r&0xFF)==PT_LAVA) && 1>(rand()%10))
+					if (((r&0xFF)==PT_FIRE || (r&0xFF)==PT_LAVA) && !(rand()%10))
 					{
 						part_change_type(i,x,y,PT_WTRV);
 					}
 				}
-	}
-	else if (t==PT_SLTW)
-	{
+		break;
+	case PT_SLTW:
 		for (rx=-2; rx<3; rx++)
 			for (ry=-2; ry<3; ry++)
-				if (x+rx>=0 && y+ry>0 &&
-						x+rx<XRES && y+ry<YRES && (rx || ry))
+				if (BOUNDS_CHECK && (rx || ry))
 				{
 					r = pmap[y+ry][x+rx];
 					if (!r)
 						continue;
-					if (((r&0xFF)==PT_FIRE || (r&0xFF)==PT_LAVA) && 1>(rand()%10))
+					if (((r&0xFF)==PT_FIRE || (r&0xFF)==PT_LAVA) && !(rand()%10))
 					{
 						if (rand()%4==0) part_change_type(i,x,y,PT_SALT);
 						else part_change_type(i,x,y,PT_WTRV);
 					}
 				}
-	}
-	else if (t==PT_DSTW)
-	{
-		for (rx=-2; rx<3; rx++)
-			for (ry=-2; ry<3; ry++)
-				if (x+rx>=0 && y+ry>0 &&
-						x+rx<XRES && y+ry<YRES && (rx || ry))
-				{
-					r = pmap[y+ry][x+rx];
-					if (!r)
-						continue;
-					if (((r&0xFF)==PT_FIRE || (r&0xFF)==PT_LAVA) && 1>(rand()%10))
-					{
-						part_change_type(i,x,y,PT_WTRV);
-					}
-				}
-	}
-	else if (t==PT_ICEI)
-	{
+		break;
+	case PT_ICEI:
 		for (rx=-2; rx<3; rx++)
 			for (ry=-2; ry<3; ry++)
 				if (BOUNDS_CHECK && (rx || ry))
@@ -88,15 +67,14 @@ int update_legacy_all(UPDATE_FUNC_ARGS)
 					r = pmap[y+ry][x+rx];
 					if (!r)
 						continue;
-					if (((r&0xFF)==PT_WATR || (r&0xFF)==PT_DSTW) && 1>(rand()%1000))
+					if (((r&0xFF)==PT_WATR || (r&0xFF)==PT_DSTW) && !(rand()%1000))
 					{
 						part_change_type(i,x,y,PT_ICEI);
 						part_change_type(r>>8,x+rx,y+ry,PT_ICEI);
 					}
 				}
-	}
-	else if (t==PT_SNOW)
-	{
+		break;
+	case PT_SNOW:
 		for (rx=-2; rx<3; rx++)
 			for (ry=-2; ry<3; ry++)
 				if (BOUNDS_CHECK && (rx || ry))
@@ -104,38 +82,31 @@ int update_legacy_all(UPDATE_FUNC_ARGS)
 					r = pmap[y+ry][x+rx];
 					if (!r)
 						continue;
-					if (((r&0xFF)==PT_WATR || (r&0xFF)==PT_DSTW) && 1>(rand()%1000))
+					if (((r&0xFF)==PT_WATR || (r&0xFF)==PT_DSTW) && !(rand()%1000))
 					{
 						part_change_type(i,x,y,PT_ICEI);
 						part_change_type(r>>8,x+rx,y+ry,PT_ICEI);
 					}
-					if (((r&0xFF)==PT_WATR || (r&0xFF)==PT_DSTW) && 15>(rand()%1000))
+					if (((r&0xFF)==PT_WATR || (r&0xFF)==PT_DSTW) && 3>(rand()%200))
 						part_change_type(i,x,y,PT_WATR);
 				}
-	}
-
-	if (t == PT_WTRV)
-	{
-		if (pv[y/CELL][x/CELL] > 4.0f)
-			part_change_type(i,x,y,PT_DSTW);
-	}
-	else if (t == PT_OIL)
-	{
+		break;
+	case PT_OIL:
 		if (pv[y/CELL][x/CELL] < -6.0f)
 			part_change_type(i,x,y,PT_GAS);
-	}
-	else if (t == PT_GAS)
-	{
+		break;
+	case PT_GAS:
 		if (pv[y/CELL][x/CELL] > 6.0f)
 			part_change_type(i,x,y,PT_OIL);
-	}
-	else if (t == PT_DESL)
-	{
+		break;
+	case PT_DESL:
 		if (pv[y/CELL][x/CELL] > 12.0f)
 		{
 			part_change_type(i,x,y,PT_FIRE);
 			parts[i].life = rand()%50+120;
 		}
+	default:
+		break;
 	}
 	return 0;
 }
@@ -162,6 +133,7 @@ int graphics_DEFAULT(GRAPHICS_FUNC_ARGS)
 	return 1;
 }
 
+//very ugly function that somehow makes powered elements floodfill update and also maintain compatibility if not
 int update_POWERED(UPDATE_FUNC_ARGS)
 {
 	int r, rx, ry;
