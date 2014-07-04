29bf483b70a25ee3cb18115e171def02466154f6
jacob1 <jfu614@gmail.com>
2014-07-04 14:49:54 -0400

    tpt++: fix URAN not heating up under pressure at 0K 3e65ed5ffd25

--- src/simulation/elements/URAN.cpp
+++ src/simulation/elements/URAN.cpp
@@ -19,8 +19,15 @@ int URAN_update(UPDATE_FUNC_ARGS)
 {
 	if (!legacy_enable && pv[y/CELL][x/CELL]>0.0f)
 	{
-		float atemp =  parts[i].temp + (-MIN_TEMP);
-		parts[i].temp = restrict_flt((atemp*(1+(pv[y/CELL][x/CELL]/2000)))+MIN_TEMP, MIN_TEMP, MAX_TEMP);
+		if (parts[i].temp == MIN_TEMP)
+		{
+			parts[i].temp += .01f;
+		}
+		else
+		{
+			float atemp =  parts[i].temp + (-MIN_TEMP);
+			parts[i].temp = restrict_flt((atemp*(1+(pv[y/CELL][x/CELL]/2000)))+MIN_TEMP, MIN_TEMP, MAX_TEMP);
+		}
 	}
 	return 0;
 }
