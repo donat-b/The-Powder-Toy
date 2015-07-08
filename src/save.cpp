0a1b43e0444caa949d0f45a4beeedf3323fbdd2f
jacob1 <jfu614@gmail.com>
2015-07-08 00:42:00 -0400

    tpt++: fix DLAY delay in older saves 7b904c68402a

--- src/save.cpp
+++ src/save.cpp
@@ -2413,8 +2413,10 @@ int parse_save_OPS(void *save, int size, int replace, int x0, int y0, unsigned c
 					{
 						if (partsptr[newIndex].type == PT_VINE)
 							partsptr[newIndex].tmp = 1;
-						if (partsptr[newIndex].type == PT_PSTN)
+						else if (partsptr[newIndex].type == PT_PSTN)
 							partsptr[newIndex].temp = 283.15;
+						else if (partsptr[newIndex].type == PT_DLAY)
+							partsptr[newIndex].temp = partsptr[newIndex].temp - 1.0f;
 					}
 					//note: PSv was used in version 77.0 and every version before, add something in PSv too if the element is that old
 
