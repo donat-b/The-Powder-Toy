ae69942d9b6aac88425f0194997ce402bfbd3c28
jacob1 <jfu614@gmail.com>
2015-11-22 01:08:48 -0500

    tpt++/jacksonmj: Fix collision detection for liquids on top of streamlines d88c18adaf86

--- src/simulation/Simulation.cpp
+++ src/simulation/Simulation.cpp
@@ -1380,7 +1380,7 @@ bool Simulation::UpdateParticle(int i)
 								break;
 							}
 							// A particle of a different type, or a wall, was found. Stop trying to move any further horizontally unless the wall should be completely invisible to particles.
-							if (bmap[ny/CELL][nx/CELL]!=WL_STREAM)
+							if ((pmap[ny][nx]&0xFF)!=t || bmap[ny/CELL][nx/CELL]!=WL_STREAM)
 								break;
 						}
 					}
@@ -1429,7 +1429,7 @@ bool Simulation::UpdateParticle(int i)
 							if ((pmap[ny][nx]&0xFF)!=t || bmap[ny/CELL][nx/CELL])
 							{
 								s = DoMove(i, clear_x, clear_y, nxf, nyf);
-								if (s || bmap[ny/CELL][nx/CELL]!=WL_STREAM)
+								if (s || (pmap[ny][nx]&0xFF)!=t || bmap[ny/CELL][nx/CELL]!=WL_STREAM)
 									break; // found the edge of the liquid and movement into it succeeded, so stop moving down
 							}
 						}
